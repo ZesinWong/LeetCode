737619de0a59d5eccdea0dcfee09d968705869e5
ZesinWong <ZesinWong@users.noreply.github.com>
2016-10-05 19:42:31 +0800

    Update ThirdSolution.java

--- Array/Find-the-Duplicate-Number/ThirdSolution.java
+++ Array/Find-the-Duplicate-Number/ThirdSolution.java
@@ -28,3 +28,24 @@ public class Solution {
 		return low;
 	}
 }
+
+public class Solution {//5ms
+	public static int findDuplicate(int[] nums) {
+		if (nums.length == 0 || nums == null)
+			return 0;
+		int low = 1, high = nums.length - 1, mid;
+		while (low < high) {
+			mid = low + (high - low) / 2;
+			int count = 0;
+			for (int i = 0; i < nums.length; i++) {
+				if (nums[i] <= mid)
+					count++;
+			}
+			if (count > mid)
+				high = mid;
+			else
+				low = mid + 1;
+		}
+		return low;
+	}
+}
