ad073c41af95a5ffe967ca9724fa7f524261f635
ZesinWong <ZesinWong@users.noreply.github.com>
2016-10-09 21:39:08 +0800

    recursive&backtracking

---
+++ Array/Combination-Sum-II/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @version 1.0
+ * @date    2016年10月9日
+ * @author  ZesinWong@gmail.com
+ */
+
+public class Solution {
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+		Arrays.sort(candidates);// step one : candidate numbers sort
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		getResult(result, new ArrayList<Integer>(), candidates, target, 0);// step two:recursive
+		return result;
+	}
+
+	public void getResult(List<List<Integer>> result, List<Integer> cur, int[] candidate, int target, int start) {
+		if (0 == target)
+			result.add(new ArrayList<Integer>(cur));
+		else if (0 < target) {
+			for (int index = start; index != candidate.length && target >= candidate[index]; ++index) {
+				if (index > start && candidate[index - 1] == candidate[index])
+					continue;
+				cur.add(candidate[index]);
+				getResult(result, cur, candidate, target - candidate[index], index+1);
+				cur.remove(cur.size() - 1);
+			} // for
+		} // else if
+	}
+}
